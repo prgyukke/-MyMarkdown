7621956b4c1eca84a8a57906f2b81fcf2311a291
Yuhsuke Gozu <yukkenottuna@gmail.com>
2019-02-27 18:30:38 +0900

    ログアウト機能実装

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,14 +1,22 @@
 <template>
-    <div class="editor">
-        <h1>エディター画面</h1>
-    </div>
+  <div class="editor">
+    <h1>エディター画面</h1>
+    <span>{{ user.displayName }}</span>
+    <button @click="logout">ログアウト</button>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "editor",
-    data() {
-        return {}
+  name: "editor",
+  props: ["user"],
+  data() {
+    return {}
+  },
+  methods: {
+    logout: function() {
+      firebase.auth().signOut()
     }
+  }
 }
 </script>
